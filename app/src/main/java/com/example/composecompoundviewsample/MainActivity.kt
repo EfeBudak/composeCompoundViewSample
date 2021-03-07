2dea98bbbed37b527da0fa28673925ae87d9a691
efebudak <efebudak1989@gmail.com>
2021-03-07 23:55:34 +0000

    Add composable Theme.kt and Color.kt Use Composable on the MainActivity.kt

--- app/src/main/java/com/example/composecompoundviewsample/MainActivity.kt
+++ app/src/main/java/com/example/composecompoundviewsample/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.example.composecompoundviewsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composecompoundviewsample.ui.ComposeCompoundViewSampleTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContent {
+            ComposeCompoundViewSampleTheme {
+                // A surface container using the 'background' color from the theme
+                ActivityMain()
+            }
+        }
     }
+}
+
+@Composable
+fun ActivityMain() {
+    ComposeCompoundViewSampleTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            Greeting("World")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ActivityMain()
 }
