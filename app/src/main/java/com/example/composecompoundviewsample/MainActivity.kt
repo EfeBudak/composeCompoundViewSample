dbbc55bac826eb13904bc78e6faafc78ce9da096
efebudak <efebudak1989@gmail.com>
2021-03-11 22:38:30 +0000

    Add Column and Counter

--- app/src/main/java/com/example/composecompoundviewsample/MainActivity.kt
+++ app/src/main/java/com/example/composecompoundviewsample/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.composecompoundviewsample
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composecompoundviewsample.ui.ComposeCompoundViewSampleTheme
 
 class MainActivity : AppCompatActivity() {
@@ -26,14 +30,29 @@ class MainActivity : AppCompatActivity() {
 fun ActivityMain() {
     ComposeCompoundViewSampleTheme {
         Surface(color = MaterialTheme.colors.background) {
-            Greeting("World")
+            Column {
+                for (i in 0..2) {
+                    Greeting("World")
+                    Divider()
+                }
+                Greeting("Mars")
+                Counter()
+            }
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", modifier = Modifier.padding(16.dp))
+}
+
+@Composable
+fun Counter(initialCount: Int = 0) {
+    val count = remember { mutableStateOf(initialCount) }
+    Button(onClick = { count.value++ }) {
+        Text("I've been clicked ${count.value} times")
+    }
 }
 
 @Preview(showBackground = true)
